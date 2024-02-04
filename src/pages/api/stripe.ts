b7302d4ce09e00a5581cbdda0d3a9ae126db63e1
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2024-02-04 15:49:26 -0200

    Update stripe.ts

--- src/pages/api/stripe.ts
+++ src/pages/api/stripe.ts
@@ -4,7 +4,7 @@ import Stripe from 'stripe';
 
 import prismadb from '../../lib/prismadb';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2023-10-16'
 });
 
