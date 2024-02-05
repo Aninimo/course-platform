4bfe631091e0c5f11e88b9889f2b08540e22161b
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2024-02-05 14:22:42 -0200

    Create sidebarRoutes.spec.tsx

---
+++ src/__test__/components/sidebarRoutes.spec.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+
+import { SidebarRoutes } from '../../components/sidebar-routes'
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      pathname: '',
+    };
+  },
+}))
+
+describe('SidebarRoutes', () => {
+  it('renders sidebar items correctly', () => {
+     render(<SidebarRoutes />)
+
+    expect(screen.getByText('Home')).toBeInTheDocument()
+    expect(screen.getByText('My courses')).toBeInTheDocument()
+    expect(screen.getByText('Notes')).toBeInTheDocument()
+    expect(screen.getByText('Settings')).toBeInTheDocument()
+  })
+})
